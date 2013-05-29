d8c50f6634081d691c6800dfb450b37483e7fa3a
SymbolixDEV <shabi_bjk@abv.bg>
2013-05-29 13:56:33 -0600

    Update npc_arena_team_top.cpp

--- src/server/scripts/Custom/npc_arena_team_top.cpp
+++ src/server/scripts/Custom/npc_arena_team_top.cpp
@@ -1,15 +1,3 @@
-/*******************************************************************************************
-*              _   _                                      __   _                           *
-*              | \ | |   ___    _ __ ___    ___    ___    / _| | |_                        *
-*              |  \| |  / _ \  | '_ ` _ \  / __|  / _ \  | |_  | __|                       *
-*              | |\  | | (_) | | | | | | | \__ \ | (_) | |  _| | |_                        *
-*              |_| \_|  \___/  |_| |_| |_| |___/  \___/  |_|    \__|                       *
-*                               The policy of Nomsoftware states: Releasing our software   *
-*                               or any other files are protected. You cannot re-release    * 
-*                               anywhere unless you were given permission.                 *
-*                           (C) Nomsoftware 2011-2012. All rights reserved.                *
-********************************************************************************************/
-
 #include "ScriptPCH.h"
 using namespace std;
 
