48f416ccdaf181ec82ca41e08281a41dbc8e40f6
SymbolixDEV <shabi_bjk@abv.bg>
2013-05-30 04:40:13 -0700

    Fix arena fast Start
    Fix by Saqirmdev

--- src/server/scripts/Custom/arena_fast_start.cpp
+++ src/server/scripts/Custom/arena_fast_start.cpp
@@ -2,20 +2,27 @@
 #include "Player.h"
 #include "Battleground.h"
 #include "BattlegroundMgr.h"
-#include <sstream> 
+#include <sstream>
 
-class fast_arena_start : public GameObjectScript
+class FastArenaCrystal : public GameObjectScript
 {
     public:
 
-        fast_arena_start() : GameObjectScript("fast_arena_start")
-        { }
+        FastArenaCrystal()
+            : GameObjectScript("FastArenaCrystal")
+        {
+        }
 
         bool OnGossipHello(Player* player, GameObject* go)
         {
             if (Battleground *bg = player->GetBattleground())
                 if (bg->isArena())
-                    ChatHandler(player->GetSession()).PSendSysMessage("Players clicked: %u", bg->ClickFastStart(player, go));
+    			{
+			           std::string msg ("Players clicked: ", bg->ClickFastStart(player, go));
+				    go->MonsterWhisper(msg.c_str(), player->GetGUID());
+				    return true;
+				}
+                    
 
             return false;
         }
@@ -23,5 +30,5 @@ class fast_arena_start : public GameObjectScript
 
 void AddSC_fast_arena_start()
 {
-    new fast_arena_start();
+    new FastArenaCrystal();
 }
